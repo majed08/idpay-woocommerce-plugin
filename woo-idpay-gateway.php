0a76820aa04d2ea5ebb218e5cdc158731b8290f8
Iman Kiani <kianiman@gmail.com>
2018-12-08 19:47:52 +0330

    enable internationalization

--- woo-idpay-gateway.php
+++ woo-idpay-gateway.php
@@ -3,7 +3,7 @@
  * Plugin Name: IDPay payment gateway for Woocommerce
  * Author: IDPay
  * Description: <a href="https://idpay.ir">IDPay</a> secure payment gateway for Woocommerce.
- * Version: 1.0.3
+ * Version: 1.0.4
  * Author URI: https://idpay.ir
  * Author Email: info@idpay.ir
  * Text Domain: woo-idpay-gateway
@@ -12,6 +12,17 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
+
+/**
+ * Load plugin textdomain.
+ *
+ * @since 1.0.0
+ */
+function woo_idpay_gateway_load_textdomain() {
+	load_plugin_textdomain( 'woo-idpay-gateway', false, basename( dirname( __FILE__ ) ) . '/languages' );
+}
+
+add_action( 'init', 'woo_idpay_gateway_load_textdomain' );
 
 require_once( 'includes/wc-geteway-idpay-helpers.php' );
 require_once( 'includes/wc-gateway-idpay-init.php' );
